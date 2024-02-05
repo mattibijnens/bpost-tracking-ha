14ebe6dc63dd19f1354639ba3e340559e6feec07
Matti <matti.bijnens+github@hotmail.com>
2024-02-05 17:30:46 +0100

    updates

--- custom_components/tracktry/sensor.py
+++ custom_components/tracktry/sensor.py
@@ -173,7 +173,7 @@ class TrackTrySensor(Entity):
     async def async_update(self, **kwargs):
         """Get the latest data from the TrackTry API."""
         _LOGGER.error("pre await")
-        await self.hass.async_add_executor_job(self.get_trackings)
+        self.get_trackings()
         _LOGGER.error("post await")
         status_to_ignore = {"delivered"}
         status_counts = {}
